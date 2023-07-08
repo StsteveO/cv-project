fe13dcf99e1198ca688e1fa0754c18a1b5ec88af
steve omari <steveomari62@gmail.com>
2023-07-08 17:58:46 -0400

    setup starting point

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+import './App.css';
+
+class App extends Component {
+  //eslint-disable-next-line
+  constructor(){
+    super()
+  }
+  //fxns
+  render(){
+    //variables
+    return (
+    <div>
+      {/* This works, and everything connects. */}
+    </div>
+  );
+  }
+}
+
+export default App;
