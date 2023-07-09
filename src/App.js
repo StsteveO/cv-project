dc320fc94b4a6116b6104ce72be2dcbd2fd5658b
steve omari <steveomari62@gmail.com>
2023-07-08 21:34:25 -0400

    connect app with components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, {Component} from "react"
 import './App.css';
+import AppHeader from "./components/AppHeader";
+import AppFooter from "./components/AppFooter";
+import CvHeader from "./components/CvHeader";
+import MainContent from "./components/MainContent";
+import SidePanel from "./components/SidePanel";
 
 class App extends Component {
   //eslint-disable-next-line
@@ -11,7 +16,25 @@ class App extends Component {
     //variables
     return (
     <div>
-      {/* This works, and everything connects. */}
+      {/* 
+      This works, and everything connects.
+      AppHeader= CV App
+      cv-header
+        name
+        job-title
+      AppFooter 
+      */}
+      <AppHeader />
+      <CvHeader
+        title="Dr." 
+        firstName="Stephen" 
+        lastName="Omari" 
+        credentials="PT, DPT" 
+        jobTitle="Physical Therapist"
+      />
+      <MainContent />
+      <SidePanel />
+      <AppFooter />
     </div>
   );
   }
