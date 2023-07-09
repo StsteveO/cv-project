1abeb908473a19332be164759708c0520efa9a86
steve omari <steveomari62@gmail.com>
2023-07-08 21:33:18 -0400

    create education section

---
+++ src/components/Education.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import SectionTitle from "./SectionTitle";
+
+//eslint-disable-next-line
+class Education extends Component {
+  //eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+  //fxns
+  render() {
+    //destruc.
+    return (
+      <>
+        <div>
+          <SectionTitle sectionName="Education" />
+        </div>
+      </>
+    );
+  }
+}
+export default Education;
