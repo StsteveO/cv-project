4048db128be5a92663f5409ee837bd5d41cb003d
steve omari <steveomari62@gmail.com>
2023-07-08 21:29:17 -0400

    create app header & footer

---
+++ src/components/AppFooter.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+//eslint-disable-next-line
+class AppFooter extends Component {
+  //eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+  //fxns
+  render() {
+    //destruc.
+    return (
+      <>
+        <div>AppFooter</div>
+      </>
+    );
+  }
+}
+export default AppFooter;
