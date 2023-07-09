76de9034e6155324f1b8def101a6f7cdc680dd07
steve omari <steveomari62@gmail.com>
2023-07-09 15:31:08 -0400

    format side panel section

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import SectionTitle from "./SectionTitle";
+import PersonalInfo from "./PersonalInfo"
+import {ReactComponent as MySvg} from "../images/HeadShotImg.svg"
 
 //eslint-disable-next-line
 class SidePanel extends Component {
@@ -10,9 +13,18 @@ class SidePanel extends Component {
   render() {
     //destruc.
     return (
-      <>
-        <div>SidePanel</div>
-      </>
+      <div className="side-panel-inner-positioning">
+        {/* <div>SidePanel</div> */}
+        {/* eslint-disable-next-line */}
+        <img />
+        <MySvg className="svg-pic" />
+        <SectionTitle sectionName="Personal Information" />
+        <PersonalInfo
+          address="123 Test Rd."
+          phoneNumber="(123) 000-0000"
+          email="test@mail.org"
+        />
+      </div>
     );
   }
 }
