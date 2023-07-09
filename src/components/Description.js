7cb96296e2793c35b83f32c57954134b83f2b405
steve omari <steveomari62@gmail.com>
2023-07-08 21:32:16 -0400

    create and fill description section

---
+++ src/components/Description.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import SectionTitle from "./SectionTitle";
+
+//eslint-disable-next-line
+class Description extends Component {
+  //eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+  //fxns
+  render() {
+    //destruc.
+    return (
+      <>
+        <div>
+          <SectionTitle sectionName="Description" />
+        </div>
+        <div className="description-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque tenetur, ex, ullam veritatis suscipit qui iure provident similique expedita officia ipsam minima soluta, sit cum! Atque quisquam ea doloribus beatae.
+        Debitis nulla aut vero error! Tenetur, ipsa at. Id facere sapiente accusamus nemo nihil cumque dicta illo quidem odio, in consequuntur eius tempora modi deleniti voluptatibus dolor amet corrupti et?
+        Consequatur, inventore omnis? Laboriosam ex illum vero numquam quo pariatur officiis, saepe iste voluptates, quis voluptate! Vero, eaque, architecto aut minus temporibus corrupti impedit similique alias officia magnam suscipit repellat?
+        Vel earum iste ratione eveniet dolore quaerat nihil? Distinctio dicta, totam odit at suscipit quis doloremque, earum ab aliquid quod autem eos labore amet similique, in omnis neque voluptatibus repudiandae?
+        Neque minus doloremque fugit, totam enim dolore perferendis, similique impedit ratione officia vitae iste reiciendis, unde deserunt explicabo alias. Maiores, ab delectus dolores sed dignissimos sit ullam minima consequatur repellat.</div>
+      </>
+    );
+  }
+}
+export default Description;
