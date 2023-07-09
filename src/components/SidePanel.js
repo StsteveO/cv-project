b23605435ad84e86286ef534f290b4caa3f360ed
steve omari <steveomari62@gmail.com>
2023-07-08 21:29:53 -0400

    create side panel

---
+++ src/components/SidePanel.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+//eslint-disable-next-line
+class SidePanel extends Component {
+  //eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+  //fxns
+  render() {
+    //destruc.
+    return (
+      <>
+        <div>SidePanel</div>
+      </>
+    );
+  }
+}
+export default SidePanel;
