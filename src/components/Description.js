bc96aa0c19b5169ef80b19b2b257d87115427c14
steve omari <steveomari62@gmail.com>
2023-07-14 22:29:25 -0400

    create and update state

--- src/components/Description.js
+++ src/components/Description.js
@@ -6,18 +6,59 @@ class Description extends Component {
   //eslint-disable-next-line
   constructor(props) {
     super(props);
+
+    this.state = {
+      descriptionModalIsShowing: true,
+      descriptionText: "",
+    };
+
+    this.toggleDescriptionModal = this.toggleDescriptionModal.bind(this);
+    this.descriptionChange = this.descriptionChange.bind(this);
   }
   //fxns
+  toggleDescriptionModal() {
+    this.setState({
+      descriptionModalIsShowing: !this.state.descriptionModalIsShowing,
+    });
+  }
+  descriptionChange(event) {
+    this.setState({
+      descriptionText: event.target.value,
+    });
+  }
+
   render() {
     //destruc.
     return (
       <>
         <div>
           <SectionTitle sectionName="Description" />
+          <button onClick={this.toggleDescriptionModal}>
+            {this.state.descriptionModalIsShowing
+              ? "Close Description Form"
+              : "Edit Description"}
+          </button>
+          {this.state.descriptionModalIsShowing && (
+            <form className="description-form">
+              <label htmlFor="descriptionBox">
+                Describe yourself professionally:
+              </label>
+              <textarea
+                id="descriptionBox"
+                name="descriptionBox"
+                placeholder="Required"
+                rows={10}
+                onChange={this.descriptionChange}
+                value={this.state.descriptionText}
+              ></textarea>
+            </form>
+          )}
         </div>
-        <div className="description-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus voluptatem minus rerum numquam consequuntur aliquid a quis suscipit, explicabo ratione error modi assumenda repellat esse animi nisi vero mollitia accusantium?
-        Et officia nostrum laboriosam animi nemo sit, pariatur consectetur, ducimus obcaecati sunt, deleniti cupiditate quod voluptate quidem doloribus! Quisquam error perferendis deserunt dolore itaque? Iste, explicabo ex. Iste, blanditiis unde.
-        Fugit earum, deserunt dicta, nihil corporis voluptatibus dolores, alias nulla aliquam excepturi molestias recusandae cum cumque maiores necessitatibus mollitia a ab. Sequi nobis iusto necessitatibus aspernatur aliquid, eligendi dolore repudiandae!</div>
+        <div className="description-content">
+          {this.state.descriptionText === ""
+            ? "[Description]"
+            : this.state.descriptionText}
+        </div>
       </>
     );
   }
