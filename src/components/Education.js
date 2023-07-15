1cc0f1ac89437143ea73d56afb7203a94144efbe
steve omari <steveomari62@gmail.com>
2023-07-14 22:31:14 -0400

    create and update state

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SectionTitle from "./SectionTitle";
+//eslint-disable-next-line
 import SectionDataOrganizer from "./SectionDataOrganizer";
 
 //eslint-disable-next-line
@@ -7,30 +8,227 @@ class Education extends Component {
   //eslint-disable-next-line
   constructor(props) {
     super(props);
+
+    this.state = {
+      // insert state here
+      educationModalIsShowing: true,
+
+      educationInfo: {
+        startDate: "",
+        endDate: "",
+        positionTitle: "",
+        companyName: "",
+        town: "",
+        state: "",
+        key: crypto.randomUUID(),
+      },
+
+      educationList: [],
+    };
+    //bind fxns here
+    this.toggleEducationModal = this.toggleEducationModal.bind(this);
+    this.startDateChange = this.startDateChange.bind(this);
+    this.endDateChange = this.endDateChange.bind(this);
+    this.positionTitleChange = this.positionTitleChange.bind(this);
+    this.companyNameChange = this.companyNameChange.bind(this);
+    this.townChange = this.townChange.bind(this);
+    this.stateChange = this.stateChange.bind(this);
+    this.submitEducationForm = this.submitEducationForm.bind(this);
   }
   //fxns
+  toggleEducationModal() {
+    this.setState({
+      educationModalIsShowing: !this.state.educationModalIsShowing,
+    });
+  }
+
+  startDateChange(event) {
+    this.setState({
+      educationInfo: {
+        startDate: event.target.value,
+        endDate: this.state.educationInfo.endDate,
+        positionTitle: this.state.educationInfo.positionTitle,
+        companyName: this.state.educationInfo.companyName,
+        town: this.state.educationInfo.town,
+        state: this.state.educationInfo.state,
+        key: this.state.educationInfo.key,
+      },
+    });
+  }
+
+  endDateChange(event) {
+    this.setState({
+      educationInfo: {
+        startDate: this.state.educationInfo.startDate,
+        endDate: event.target.value,
+        positionTitle: this.state.educationInfo.positionTitle,
+        companyName: this.state.educationInfo.companyName,
+        town: this.state.educationInfo.town,
+        state: this.state.educationInfo.state,
+        key: this.state.educationInfo.key,
+      },
+    });
+  }
+
+  positionTitleChange(event) {
+    this.setState({
+      educationInfo: {
+        startDate: this.state.educationInfo.startDate,
+        endDate: this.state.educationInfo.endDate,
+        positionTitle: event.target.value,
+        companyName: this.state.educationInfo.companyName,
+        town: this.state.educationInfo.town,
+        state: this.state.educationInfo.state,
+        key: this.state.educationInfo.key,
+      },
+    });
+  }
+
+  companyNameChange(event) {
+    this.setState({
+      educationInfo: {
+        startDate: this.state.educationInfo.startDate,
+        endDate: this.state.educationInfo.endDate,
+        positionTitle: this.state.educationInfo.positionTitle,
+        companyName: event.target.value,
+        town: this.state.educationInfo.town,
+        state: this.state.educationInfo.state,
+        key: this.state.educationInfo.key,
+      },
+    });
+  }
+
+  townChange(event) {
+    this.setState({
+      educationInfo: {
+        startDate: this.state.educationInfo.startDate,
+        endDate: this.state.educationInfo.endDate,
+        positionTitle: this.state.educationInfo.positionTitle,
+        companyName: this.state.educationInfo.companyName,
+        town: event.target.value,
+        state: this.state.educationInfo.state,
+        key: this.state.educationInfo.key,
+      },
+    });
+  }
+
+  stateChange(event) {
+    this.setState({
+      educationInfo: {
+        startDate: this.state.educationInfo.startDate,
+        endDate: this.state.educationInfo.endDate,
+        positionTitle: this.state.educationInfo.positionTitle,
+        companyName: this.state.educationInfo.companyName,
+        town: this.state.educationInfo.town,
+        state: event.target.value,
+        key: this.state.educationInfo.key,
+      },
+    });
+  }
+
+  submitEducationForm(event) {
+    event.preventDefault();
+    this.setState({
+      educationList: [this.state.educationInfo, ...this.state.educationList],
+
+      educationInfo: {
+        startDate: "",
+        endDate: "",
+        positionTitle: "",
+        companyName: "",
+        town: "",
+        state: "",
+        key: crypto.randomUUID(),
+      },
+    });
+
+    // console.log(this.state.educationList);
+  }
+
   render() {
     //destruc.
     return (
       <>
         <div>
           <SectionTitle sectionName="Education" />
-          <SectionDataOrganizer
-            startDate="8/2014"
-            endDate="5/2017"
-            positionTitle="Doctorate of Physical Therapy"
-            facilityName="Rutgers University"
-            city="Newark"
-            state="NJ"
-          />
-          <SectionDataOrganizer
-            startDate="8/2012"
-            endDate="5/2015"
-            positionTitle="Bachelor of Science"
-            facilityName="Felician University"
-            city="Rutherford"
-            state="NJ"
-          />
+          <button onClick={this.toggleEducationModal}>
+            {this.state.educationModalIsShowing
+              ? "Close Experience Form"
+              : "Add Education"}
+          </button>
+          {this.state.educationModalIsShowing && (
+            <form
+              className="education-form"
+              onSubmit={this.submitEducationForm}
+            >
+              <label htmlFor="startDate">Start Date:</label>
+              <input
+                id="startDate"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.startDateChange}
+                value={this.state.educationInfo.startDate}
+              />
+
+              <label htmlFor="endDate">End Date:</label>
+              <input
+                id="endDate"
+                placeholder="Leave Blank if Still Working"
+                type="text"
+                // required
+                onChange={this.endDateChange}
+                value={this.state.educationInfo.endDate}
+              />
+
+              <label htmlFor="positionTitle">Title of Major:</label>
+              <input
+                id="positionTitle"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.positionTitleChange}
+                value={this.state.educationInfo.positionTitle}
+              />
+
+              <label htmlFor="companyName">College/University Name:</label>
+              <input
+                id="companyName"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.companyNameChange}
+                value={this.state.educationInfo.companyName}
+              />
+
+              <label htmlFor="town">Town:</label>
+              <input
+                id="town"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.townChange}
+                value={this.state.educationInfo.town}
+              />
+
+              <label htmlFor="state">State (abbv):</label>
+              <input
+                id="state"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.stateChange}
+                value={this.state.educationInfo.state}
+              />
+
+              <div className="multiple-btns">
+                <button type="submit">Add Education</button>
+                <button type="reset">Cancel</button>
+              </div>
+            </form>
+          )}
+
+          <SectionDataOrganizer list={this.state.educationList} />
         </div>
       </>
     );
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,37 +7,228 @@ class Experience extends Component {
   //eslint-disable-next-line
   constructor(props) {
     super(props);
+
+    this.state = {
+      // insert state here
+      experienceModalIsShowing: true,
+
+      experienceInfo: {
+        startDate: "",
+        endDate: "",
+        positionTitle: "",
+        companyName: "",
+        town: "",
+        state: "",
+        key: crypto.randomUUID(),
+      },
+
+      experienceList: [],
+    };
+    // bind fxns here
+    this.toggleExperienceModal = this.toggleExperienceModal.bind(this);
+    this.startDateChange = this.startDateChange.bind(this);
+    this.endDateChange = this.endDateChange.bind(this);
+    this.positionTitleChange = this.positionTitleChange.bind(this);
+    this.companyNameChange = this.companyNameChange.bind(this);
+    this.townChange = this.townChange.bind(this);
+    this.stateChange = this.stateChange.bind(this);
+    this.submitExperienceForm = this.submitExperienceForm.bind(this);
   }
   //fxns
+  toggleExperienceModal() {
+    this.setState({
+      experienceModalIsShowing: !this.state.experienceModalIsShowing,
+    });
+  }
+
+  startDateChange(event) {
+    this.setState({
+      experienceInfo: {
+        startDate: event.target.value,
+        endDate: this.state.experienceInfo.endDate,
+        positionTitle: this.state.experienceInfo.positionTitle,
+        companyName: this.state.experienceInfo.companyName,
+        town: this.state.experienceInfo.town,
+        state: this.state.experienceInfo.state,
+        key: this.state.experienceInfo.key,
+      },
+    });
+  }
+
+  endDateChange(event) {
+    this.setState({
+      experienceInfo: {
+        startDate: this.state.experienceInfo.startDate,
+        endDate: event.target.value,
+        positionTitle: this.state.experienceInfo.positionTitle,
+        companyName: this.state.experienceInfo.companyName,
+        town: this.state.experienceInfo.town,
+        state: this.state.experienceInfo.state,
+        key: this.state.experienceInfo.key,
+      },
+    });
+  }
+
+  positionTitleChange(event) {
+    this.setState({
+      experienceInfo: {
+        startDate: this.state.experienceInfo.startDate,
+        endDate: this.state.experienceInfo.endDate,
+        positionTitle: event.target.value,
+        companyName: this.state.experienceInfo.companyName,
+        town: this.state.experienceInfo.town,
+        state: this.state.experienceInfo.state,
+        key: this.state.experienceInfo.key,
+      },
+    });
+  }
+
+  companyNameChange(event) {
+    this.setState({
+      experienceInfo: {
+        startDate: this.state.experienceInfo.startDate,
+        endDate: this.state.experienceInfo.endDate,
+        positionTitle: this.state.experienceInfo.positionTitle,
+        companyName: event.target.value,
+        town: this.state.experienceInfo.town,
+        state: this.state.experienceInfo.state,
+        key: this.state.experienceInfo.key,
+      },
+    });
+  }
+
+  townChange(event) {
+    this.setState({
+      experienceInfo: {
+        startDate: this.state.experienceInfo.startDate,
+        endDate: this.state.experienceInfo.endDate,
+        positionTitle: this.state.experienceInfo.positionTitle,
+        companyName: this.state.experienceInfo.companyName,
+        town: event.target.value,
+        state: this.state.experienceInfo.state,
+        key: this.state.experienceInfo.key,
+      },
+    });
+  }
+
+  stateChange(event) {
+    this.setState({
+      experienceInfo: {
+        startDate: this.state.experienceInfo.startDate,
+        endDate: this.state.experienceInfo.endDate,
+        positionTitle: this.state.experienceInfo.positionTitle,
+        companyName: this.state.experienceInfo.companyName,
+        town: this.state.experienceInfo.town,
+        state: event.target.value,
+        key: this.state.experienceInfo.key,
+      },
+    });
+  }
+
+  submitExperienceForm(event) {
+    event.preventDefault();
+    this.setState({
+      experienceList: [this.state.experienceInfo, ...this.state.experienceList],
+
+      experienceInfo: {
+        startDate: "",
+        endDate: "",
+        positionTitle: "",
+        companyName: "",
+        town: "",
+        state: "",
+        key: crypto.randomUUID(),
+      },
+    });
+
+    // console.log(this.state.experienceList);
+  }
+
   render() {
     //destruc.
     return (
       <>
         <div>
           <SectionTitle sectionName="Experience" />
+          <button onClick={this.toggleExperienceModal}>
+            {this.state.experienceModalIsShowing
+              ? "Close Experience Form"
+              : "Add Experience"}
+          </button>
+          {this.state.experienceModalIsShowing && (
+            <form
+              className="experience-form"
+              onSubmit={this.submitExperienceForm}
+            >
+              <label htmlFor="startDate">Start Date:</label>
+              <input
+                id="startDate"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.startDateChange}
+                value={this.state.experienceInfo.startDate}
+              />
+
+              <label htmlFor="endDate">End Date:</label>
+              <input
+                id="endDate"
+                placeholder="Leave Blank if Still Working"
+                type="text"
+                // required
+                onChange={this.endDateChange}
+                value={this.state.experienceInfo.endDate}
+              />
+
+              <label htmlFor="positionTitle">Position Title:</label>
+              <input
+                id="positionTitle"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.positionTitleChange}
+                value={this.state.experienceInfo.positionTitle}
+              />
+
+              <label htmlFor="companyName">Company Name:</label>
+              <input
+                id="companyName"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.companyNameChange}
+                value={this.state.experienceInfo.companyName}
+              />
+
+              <label htmlFor="town">Town:</label>
+              <input
+                id="town"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.townChange}
+                value={this.state.experienceInfo.town}
+              />
+
+              <label htmlFor="state">State (abbv):</label>
+              <input
+                id="state"
+                placeholder="Required"
+                type="text"
+                required
+                onChange={this.stateChange}
+                value={this.state.experienceInfo.state}
+              />
+
+              <div className="multiple-btns">
+                <button type="submit">Add Experience</button>
+                <button type="reset">Cancel</button>
+              </div>
+            </form>
+          )}
+
           <SectionDataOrganizer
-            startDate="7/2018"
-            endDate=""
-            positionTitle="Physical Therapist"
-            facilityName="Virtua Home Health"
-            city="Mount Laurel"
-            state="NJ"
-          />
-          <SectionDataOrganizer
-            startDate="4/2018"
-            endDate="5/2020"
-            positionTitle="Physical Therapist"
-            facilityName="CareOne"
-            city="Moorestown"
-            state="NJ"
-          />
-          <SectionDataOrganizer
-            startDate="9/2017"
-            endDate="10/2018"
-            positionTitle="Physical Therapist"
-            facilityName="FitRehab"
-            city="Voorhees Township"
-            state="NJ"
+            list={this.state.experienceList}
           />
         </div>
       </>
